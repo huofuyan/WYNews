eaf68d84e8280a8b3c8e14c72f7708faec3c2082
霍福岩 <527590071@qq.com>
2016-06-17 11:51:53 +0800

    初始化项目

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by 霍福岩 on 16/6/17.
+//  Copyright © 2016年 霍福岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/网易新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  网易新闻
+//
+//  Created by 霍福岩 on 16/6/17.
+//  Copyright © 2016年 霍福岩. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
